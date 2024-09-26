5a6478649ef7f1d3429c1b87a1ea22030d1d82f3
Mason Matray <mjmatray@icloud.com>
2024-09-26 12:33:42 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,5 +1,4 @@
 <?php
-function get_db_connection(){
 function get_db_connection() {
     // Define Azure database credentials
     $servername = "dbhw3.mysql.database.azure.com"; // Azure server hostname
@@ -7,15 +6,15 @@ function get_db_connection() {
     $password = "dbload2024!"; // Your Azure MySQL password
     $dbname = "hw3db"; // Your database name
     $port = 3306; // MySQL default port
+
     // Create connection
-    $conn = new mysqli('138.197.17.168', 'projecto_hw3user', 'Opr%wFjet!Lr', 'projecto_hw3');
-    
     $conn = new mysqli($servername, $username, $password, $dbname, $port);
+
     // Check connection
     if ($conn->connect_error) {
-      return false;
         die("Connection failed: " . $conn->connect_error);
     }
+
     return $conn;
 }
 ?>
