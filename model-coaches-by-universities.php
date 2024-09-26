e8de60e231d1239c070d6fc03d00dee5dc9b9b46
Mason Matray <mjmatray@icloud.com>
2024-09-26 13:38:45 -0500

    Create model-coaches-by-universities.php

---
+++ model-coaches-by-universities.php
@@ -0,0 +1,16 @@
+<?php
+function selectCoachesByUniversities($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT u.university_id, university_location, university_mascot, university_name, conference, first_opponent, record FROM university U JOIN team t ON U.university_id = t.university_id WHERE t.coach_id = ?");
+        $stmt->bind_param("c", $cid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
