95d53fe92eca178e4f9604f2f32a8d5690e7720c
Mason Matray <mjmatray@icloud.com>
2024-09-26 11:38:48 -0500

    Create view-universities.php

---
+++ view-universities.php
@@ -0,0 +1,27 @@
+<h1>Universities</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Location</th>
+      <th>Mascot</th>
+      <th>Name</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($coach = $coaches->fetch_assoc()) {
+  ?>
+  <tr>
+    <td><?php echo $university['university_id']; ?></td>
+    <td><?php echo $university['university_location']; ?></td>
+    <td><?php echo $university['university_mascot']; ?></td>
+    <td><?php echo $university['university_name']; ?></td>
+  </tr>
+  <?php
+}
+?>
+    </tbody>
+  </table>
+</div>
