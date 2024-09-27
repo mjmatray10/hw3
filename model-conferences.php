27c23f3adaa96aa8b3e1d6db4435d425a604f6b1
Mason Matray <mjmatray@icloud.com>
2024-09-27 14:56:48 -0500

    Update model-conferences.php

--- model-conferences.php
+++ model-conferences.php
@@ -2,25 +2,7 @@
 function selectConferences() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT DISTINCT conference FROM conference");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $stmt->close(); 
-        return $result;
-    } catch (Exception $e) {
-        throw $e;
-    } finally {
-        if ($conn) {
-            $conn->close();
-        }
-    }
-}
-
-function selectTeamsByConference($conference) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT u.university_id, u.university_name FROM university u JOIN conference c ON u.university_id = c.university_id WHERE c.conference = ?");
-        $stmt->bind_param("s", $conference);
+        $stmt = $conn->prepare("SELECT conference_id, conference FROM conference");
         $stmt->execute();
         $result = $stmt->get_result();
         $stmt->close();
@@ -33,4 +15,24 @@ function selectTeamsByConference($conference) {
         }
     }
 }
+
+function selectTeamsByConferenceId($conference_id) {
+        try {
+            $conn = get_db_connection();
+            $stmt = $conn->prepare("SELECT u.university_id, u.university_name FROM university u JOIN conference c ON u.university_id = c.university_id WHERE c.conference_id = ?");
+            $stmt->bind_param("i", $conference_id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $stmt->close();
+            return $result;
+        } catch (Exception $e) {
+            throw $e;
+        } finally {
+            if ($conn) {
+                $conn->close();
+            }
+        }
+    }
+
+    $teams = selectTeamsByConferenceId($conference_id);
 ?>
