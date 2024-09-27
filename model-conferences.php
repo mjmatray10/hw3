c793abf7ca2efff6f584d0016d58f985a62cd9ea
Mason Matray <mjmatray@icloud.com>
2024-09-27 14:25:52 -0500

    Update model-conferences.php

--- model-conferences.php
+++ model-conferences.php
@@ -3,6 +3,7 @@ function selectConferences() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("select university_name, conference from university u join team t on u.university_id = t.university_id where conference = ?");
+        $stmt->bind_param("i", $cid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -10,6 +11,24 @@ function selectConferences() {
     } catch (Exception $e) {
         $conn->close();
         throw $e;
+    }
+}
+
+function selectTeamsByConference($conference) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT university_id, university_name FROM university WHERE conference = ?");
+        $stmt->bind_param("s", $conference);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        return $result;
+    } catch (Exception $e) {
+        throw $e;
+    } finally {
+        if ($conn) {
+            $conn->close();
+        }
     }
 }
 ?>
