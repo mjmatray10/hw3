6481d45d772ea742f5439b9bb5cb561e11d94a49
Mason Matray <mjmatray@icloud.com>
2024-09-27 15:29:19 -0500

    Update model-programs-by-university.php

--- model-programs-by-university.php
+++ model-programs-by-university.php
@@ -2,7 +2,7 @@
 function selectProgramsByUniversity($uid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT u.university_id, university_location, university_mascot, university_name, conference, first_opponent, record FROM university U JOIN team t ON U.university_id = t.university_id WHERE t.university_id = ?");
+        $stmt = $conn->prepare("SELECT u.university_id, university_location, university_mascot, university_name, conference, first_opponent, record FROM 'university' U JOIN team t ON U.university_id = t.university_id WHERE t.university_id = ?");
         $stmt->bind_param("i", $uid);
         $stmt->execute();
         $result = $stmt->get_result();
