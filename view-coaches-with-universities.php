3e55c0aadf05f2a72dc29144e45faf3da063587b
Mason Matray <mjmatray@icloud.com>
2024-09-27 11:50:20 -0500

    Update view-coaches-with-universities.php

--- view-coaches-with-universities.php
+++ view-coaches-with-universities.php
@@ -4,23 +4,21 @@
 while ($coach = $coaches->fetch_assoc()) {
   ?>
     <div class="card">
-    <div class="card-body">
-      <h5 class="card-title"><?php echo $coach['coach_name']; ?></h5>
-      <p class="card-text">
-      <ul class="list-group">
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $coach['coach_name']; ?></h5>
+        <ul class="list-group">
 <?php
   $universities = selectCoachesByUniversity($coach['coach_id']);
   while ($university = $universities->fetch_assoc()) {
 ?>
-    <li class="list-group-item"><?php echo $program['university_name']; ?> - <?php echo $program['record']; ?> - <?php echo $program['conference']; ?> - <?php echo $program['university_location']; ?></li>
+    <li class="list-group-item"><?php echo $university['university_name']; ?> - <?php echo $university['record']; ?> - <?php echo $university['conference']; ?> - <?php echo $university['university_location']; ?></li>
 <?php    
   }
 ?>
-      </ul>
-      </p>
-      <p class="card-text"><small class="text-body-secondary">Current Team: <?php echo $coach['coach_team']; ?></small></p>
+        </ul>
+        <p class="card-text"><small class="text-body-secondary">Current Team: <?php echo $coach['coach_team']; ?></small></p>
+      </div>
     </div>
-  </div>
   <?php
 }
 ?>
