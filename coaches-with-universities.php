38eccc5263a4c6b6fb0a1e4b66cd5902f0dc09e0
Mason Matray <mjmatray@icloud.com>
2024-09-27 11:28:09 -0500

    Update coaches-with-universities.php

--- coaches-with-universities.php
+++ coaches-with-universities.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-coaches-with-universities.php");
 
+$pageTitle = "Coaches with Universities";
+include "view-header.php";
+$coaches = selectCoaches();
+include "view-coaches-with-universities.php";
+include "view-footer.php";
+?>
