8b8fb3d5da4faf32ab339b2feac895f1e2bb8259
Mason Matray <mjmatray@icloud.com>
2024-09-27 16:22:58 -0500

    Update teams-by-conference.php

--- teams-by-conference.php
+++ teams-by-conference.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-teams-by-conference.php");
 $pageTitle = "Teams by Conference";
 include "view-header.php";
-$teams = $_POST['ccid'];
+$teams = selectTeamsByConferenceId($_GET['ccid']);
 include "view-teams-by-conference.php";
 include "view-footer.php";
 ?>
