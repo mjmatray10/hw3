b09c24326180e2198569827e76b842f60eddd0ce
Mason Matray <mjmatray@icloud.com>
2024-09-27 16:19:37 -0500

    Update model-teams-by-conference.php

--- model-teams-by-conference.php
+++ model-teams-by-conference.php
@@ -3,7 +3,7 @@ function selectTeamsByConferenceId($conference_id) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT u.university_id, u.university_name FROM university u JOIN team t ON u.university_id = t.university_id WHERE t.conference_id = ?");
-        $stmt->bind_param("i", $conference_id);
+        $stmt->bind_param("i", $ccid);
         $stmt->execute();
         $result = $stmt->get_result();
         $stmt->close();
