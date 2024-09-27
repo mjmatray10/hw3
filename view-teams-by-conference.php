c05724c4ae93535ff87978d2a1e5bfb779e4ccbd
Mason Matray <mjmatray@icloud.com>
2024-09-27 15:18:10 -0500

    Update view-teams-by-conference.php

--- view-teams-by-conference.php
+++ view-teams-by-conference.php
@@ -1,26 +1,23 @@
 <h1>Teams in Conference</h1>
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                </tr>
-            </thead>
-            <tbody>
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+            </tr>
+        </thead>
+        <tbody>
 <?php
-    while ($team = $teams->fetch_assoc()) {
+while ($team = $teams->fetch_assoc()) {
 ?>
-                <tr>
-                    <td><?php echo $team['university_id']; ?></td>
-                    <td><?php echo $team['university_name']; ?></td>
-                </tr>
-<?php
-    }
-?>
-            </tbody>
-        </table>
-    </div>
+            <tr>
+                <td><?php echo $team['university_id']; ?></td>
+                <td><?php echo $team['university_name']; ?></td>
+            </tr>
 <?php
 }
 ?>
+        </tbody>
+    </table>
+</div>
