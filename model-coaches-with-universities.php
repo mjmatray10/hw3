b62d20be9ac46f676849ff6b3a874dda27fa4c3a
Mason Matray <mjmatray@icloud.com>
2024-09-27 11:30:59 -0500

    Update model-coaches-with-universities.php

--- model-coaches-with-universities.php
+++ model-coaches-with-universities.php
@@ -12,4 +12,19 @@ function selectCoaches() {
         throw $e;
     }
 }
+
+function selectCoachesByUniversities($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT u.university_id, university_location, university_mascot, university_name, conference, first_opponent, record FROM university U JOIN team t ON U.university_id = t.university_id WHERE t.coach_id = ?");
+        $stmt->bind_param("i", $cid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
