7a68ad35048f7942570feefac9a328b92983e263
Mason Matray <mjmatray@icloud.com>
2024-09-27 15:10:50 -0500

    Create teams-by-conference.php

---
+++ teams-by-conference.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-teams-by-conference.php");
+
+$pageTitle = "Teams by Conference";
+include "view-header.php";
+$conference_id = $_POST['conference_id'];
+include "view-teams-by-conference.php";
+include "view-footer.php";
+?>
