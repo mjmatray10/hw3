35d4f82c14f67404d4711f7ead2fe4fedb54a8cd
Mason Matray <mjmatray@icloud.com>
2024-09-27 16:06:42 -0500

    Update teams-by-conference.php

--- teams-by-conference.php
+++ teams-by-conference.php
@@ -1,8 +1,12 @@
 <?php
 require_once("util-db.php");
 require_once("model-teams-by-conference.php");
-$conference_id = $_POST['conference_id'];
+if (!isset($_POST['conference_id']) || empty($_POST['conference_id'])) {
+    die("Error: Conference ID is not set or is empty.");
+}
+$conference_id = intval($_POST['conference_id']);
 $teams = selectTeamsByConferenceId($conference_id);
+
 $pageTitle = "Teams by Conference";
 include "view-header.php";
 include "view-teams-by-conference.php";
