f3bb0b203a9d1f398de8b7f31e378343a5f6aeda
Mason Matray <mjmatray@icloud.com>
2024-10-18 13:41:02 -0500

    Update universities.php

--- universities.php
+++ universities.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
           echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
+    case "Delete":
+      if (deleteUniversities($_POST['uid'])) {
+        echo '<div class="alert alert-success" role="alert">University Deleted</div>';
+      } else {
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
   }
 }
 
