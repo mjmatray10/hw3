2acf7272e6a98f04eea9f47e59ae0600ebbd0673
Mason Matray <mjmatray@icloud.com>
2024-10-18 18:02:03 -0500

    Update teams.php

--- teams.php
+++ teams.php
@@ -8,21 +8,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch($_POST['actionType']) {
     case "Add":
-      if (insertTeams($_POST['tFO'], $_POST['tcoachid'], $_POST['tuid'], $_POST['tcon'],$_POST['trec'], $_POST['tconid'])) {
+      if (insertTeams($_POST['tcoachid'], $_POST['tuid'], $_POST['tcon'], $_POST['tFO'], $_POST['trec'], $_POST['tconid'])) {
         echo '<div class="alert alert-success" role="alert">Team Added</div>';
       } else {
           echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
     case "Edit":
-      if (updateTeams($_POST['tFO'], $_POST['tcoachid'], $_POST['tuid'], $_POST['tcon'],$_POST['trec'], $_POST['tconid'], $_POST['tid'])) {
+      if (updateTeams($_POST['tcoachid'], $_POST['tuid'], $_POST['tcon'],$_POST['tFO'], $_POST['trec'], $_POST['tconid'], $_POST['tid'])) {
         echo '<div class="alert alert-success" role="alert">Team Edited</div>';
       } else {
           echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
       break;
     case "Delete":
-      if (deleteTeams($_POST['cid'])) {
+      if (deleteTeams($_POST['tid'])) {
         echo '<div class="alert alert-success" role="alert">Team Deleted</div>';
       } else {
           echo '<div class="alert alert-danger" role="alert">Error</div>';
