6d4070d39ba7a1655ab486b693cf1c9779584929
Mason Matray <mjmatray@icloud.com>
2024-10-18 15:10:28 -0500

    Update conferences.php

--- conferences.php
+++ conferences.php
@@ -4,6 +4,33 @@ require_once("model-conferences.php");
 
 $pageTitle = "Conferences";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch($_POST['actionType']) {
+    case "Add":
+      if (insertConferences($_POST['conName'])) {
+        echo '<div class="alert alert-success" role="alert">Conference Added</div>';
+      } else {
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Edit":
+      if (updateConferences($_POST['conName'], $_POST['conid'])) {
+        echo '<div class="alert alert-success" role="alert">Conference Edited</div>';
+      } else {
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteCoaches($_POST['conid'])) {
+        echo '<div class="alert alert-success" role="alert">Conference Deleted</div>';
+      } else {
+          echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+  }
+}
+
 $coaches = selectConferences();
 include "view-conferences.php";
 include "view-footer.php";
