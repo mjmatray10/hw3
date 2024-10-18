6bc7ae2cf6c1e8e1b1cef2c3c7422fee984f0cd0
Mason Matray <mjmatray@icloud.com>
2024-10-18 12:55:55 -0500

    Update model-universities.php

--- model-universities.php
+++ model-universities.php
@@ -12,4 +12,47 @@ function selectUniversities() {
         throw $e;
     }
 }
+
+function insertUniversities($uLoc, $uMas, $uName) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `university` (`university_location`, `university_mascot`,  `university_name`) VALUES (?,?,?)");
+        $stmt->bind_param("sss", $uLoc, $uMas, $uName);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateUniversities($uLoc, $uMas, $uName, $uid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `university` set `university_location` = ?, `university_mascot` = ?,  `university_name` = ? where university_id = ?");
+        $stmt->bind_param("sssi", $uLoc, $uMas, $uName, $uid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteUniversities($uid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from universities where university_id = ?");
+        $stmt->bind_param("i", $uid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
