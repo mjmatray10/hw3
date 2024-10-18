f07f8062e0d67cdb87f2ebb164eb7baa7008041b
Mason Matray <mjmatray@icloud.com>
2024-10-18 14:19:45 -0500

    Update model-coaches.php

--- model-coaches.php
+++ model-coaches.php
@@ -12,4 +12,47 @@ function selectCoaches() {
         throw $e;
     }
 }
+
+function insertCoaches($cName, $cTeam) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `coach` (`coach_name`, `coach_university`) VALUES (?,?)");
+        $stmt->bind_param("ss", $cName, $cTeam);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateCoaches($cName, $cTeam, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `coach` set `coach_name` = ?, `coach_team` = ? where coach_id = ?");
+        $stmt->bind_param("ssi", $cName, $cTeam, $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCoaches($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from coach where coach_id = ?");
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 ?>
