0e36a57923c0336c1f8577dd8a80f13bf42a8cf5
Mason Matray <mjmatray@icloud.com>
2024-10-18 17:32:36 -0500

    Create model-teams.php

---
+++ model-teams.php
@@ -0,0 +1,58 @@
+<?php
+function selectTeams() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT team_id, coach_id, university_id, conference, first_opponent, record, conference_id FROM `team`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertTeams($tcoachid, $tuid, $tcon, $tFO, $trec, $tconid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO team (team_id, coach_id, university_id, conference, first_opponent, record, conference_id) VALUES (?,?,?,?,?,?,?)");
+        $stmt->bind_param("iisssi", $tcoachid, $tuid, $tcon, $tFO, $trec, $tconid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateTeams($tcoachid, $tuid, $tcon, $tFO, $trec, $tconid, $tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `team` set `team_id` = ?, `coach_id` = ?, `university_id` = ?, `conference` = ?, `first_opponent` = ?, `record` = ?, `conference_id` = ? where team_id = ?");
+        $stmt->bind_param("iisssii", $tcoachid, $tuid, $tcon, $tFO, $trec, $tconid, $tid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteTeams($tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from team where team_id = ?");
+        $stmt->bind_param("i", $tid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+?>
