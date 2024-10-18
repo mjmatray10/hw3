0666449cb1ee462154c35d5d40c6fbc83d86e903
Mason Matray <mjmatray@icloud.com>
2024-10-18 16:14:37 -0500

    Update view-conferences.php

--- view-conferences.php
+++ view-conferences.php
@@ -33,8 +33,8 @@ while ($conference = $allConferences->fetch_assoc()) {
       ?>
     </td>
     <td>
-       <form method="post", action="">
-          <input type="hidden" name="conid", value="<?php echo $conference['conference_id']; ?>">
+       <form method="post" action="">
+          <input type="hidden" name="conid" value="<?php echo $conference['conference_id']; ?>">
          <input type="hidden" name="actionType" value="Delete">
           <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-x" viewBox="0 0 16 16">
