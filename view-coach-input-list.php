8551c499251d9dbd2a36a42d46c871f72ed21174
Mason Matray <mjmatray@icloud.com>
2024-10-20 19:33:46 -0500

    Update view-coach-input-list.php

--- view-coach-input-list.php
+++ view-coach-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="cid" name="cid">
 <?php
 while ($coachItem = $coachList->fetch_assoc()) {
+  $selText = "";
+  if ($selectedCoach == $coachItem['coach_id']){
+    $selText = " Selected";
+  }
   ?>
-    <option value="<?php echo $coachItem['coach_id']; ?>"><?php echo $coachItem['coach_name']; ?></option>
+    <option value="<?php echo $coachItem['coach_id']; ?>"<?=$selText?>><?php echo $coachItem['coach_name']; ?></option>
   <?php
 }
 ?>
