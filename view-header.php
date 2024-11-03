40137e7003233de4e5a007b2b7150ad133605f5b
Mason Matray <mjmatray@icloud.com>
2024-11-03 14:49:10 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -31,6 +31,9 @@
         <li class="nav-item">
           <a class="nav-link" href="conferences.php">Conferences</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript.php">Javascript</a>
+        </li>
       </ul>
     </div>
   </div>
