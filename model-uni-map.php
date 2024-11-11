4920f177deec04f691eb92f158d622c8f16080af
Mason Matray <mjmatray@icloud.com>
2024-11-11 14:30:15 -0600

    Create model-uni-map.php

---
+++ model-uni-map.php
@@ -0,0 +1,15 @@
+<?php
+function selectUniversityLocations() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT university_name, location FROM university");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
