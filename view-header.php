df74605ab46dffe034fcedeaf01629fa03fab982
Mason Matray <mjmatray@icloud.com>
2024-11-11 14:34:00 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -40,6 +40,9 @@
         <li class="nav-item">
           <a class="nav-link" href="coaches-bar.php">Coach Bar Chart</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="uni-map.php">University Map</a>
+        </li>
       </ul>
     </div>
   </div>
