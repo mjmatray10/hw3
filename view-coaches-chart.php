c6e080898e1e66f03f80526b271ca0c8a3225163
Mason Matray <mjmatray@icloud.com>
2024-11-11 13:17:23 -0600

    Update view-coaches-chart.php

--- view-coaches-chart.php
+++ view-coaches-chart.php
@@ -1,6 +1,40 @@
 <h1>Coach Chart</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 <?php
 while ($coach = $coaches->fetch_assoc()) {
   
 }
 ?>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+    datasets: [{
+        data: [
+<?php
+while ($coach = $coaches->fetch_assoc()) {
+  echo $coach['num_unis'] . ", ";
+}
+?>
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+<?php
+$coaches = selectCoaches();
+while ($coach = $coaches->fetch_assoc()) {
+  echo "'" . $coach['coach_name'] . "', ";
+}
+?>
+    ]
+},
+  });
+</script>
