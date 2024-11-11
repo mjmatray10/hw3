baa67fb888da3c0c90589b5eeffff1fb9fe8090e
Mason Matray <mjmatray@icloud.com>
2024-11-11 14:23:02 -0600

    Update model-coaches-bar-db.php

--- model-coaches-bar-db.php
+++ model-coaches-bar-db.php
@@ -1,10 +1,15 @@
 <?php
-require_once("util-db.php");
-require_once("model-coaches-chart-db.php");
-
-$pageTitle = "Coach Chart";
-include "view-header.php";
-$coaches = selectCoaches();
-include "view-coaches-chart.php";
-include "view-footer.php";
+function selectCoaches() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT coach_name, count(u.university_id) as num_unis FROM `coach` c JOIN university u on c.coach_id = u.coach_id group by coach_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
