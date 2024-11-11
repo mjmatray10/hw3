d57988e4efd467bfd804615e6b1810f3f3fea84e
Mason Matray <mjmatray@icloud.com>
2024-11-11 14:55:50 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -43,6 +43,9 @@
         <li class="nav-item">
           <a class="nav-link" href="uni-map.php">University Map</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="record-table.php">Record DataTables</a>
+        </li>
       </ul>
     </div>
   </div>
