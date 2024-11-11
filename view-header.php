3e7f6c3d68c5f12b2fefe90e46db5bc8bb7de5cd
Mason Matray <mjmatray@icloud.com>
2024-11-11 13:43:12 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -34,6 +34,9 @@
         <li class="nav-item">
           <a class="nav-link" href="javascript.php">Javascript</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="view-coaches-chart.php">Coach Chart</a>
+        </li>
       </ul>
     </div>
   </div>
