dacbba4cd75808dd54e19480031ea6ac41b7d8a5
Mason Matray <mjmatray@icloud.com>
2024-11-11 14:59:05 -0600

    Create model-record-table-db.php

---
+++ model-record-table-db.php
@@ -0,0 +1,13 @@
+function selectUniversityRecords() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT university_name, t.record FROM team t JOIN university u ON t.university_id = u.university_id");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
