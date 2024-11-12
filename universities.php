133aca293456d25d64764eb54bc778951cb5b509
Mason Matray <mjmatray@icloud.com>
2024-11-11 18:15:30 -0600

    Update universities.php

--- universities.php
+++ universities.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch($_POST['actionType']) {
     case "Add":
-      if (insertUniversities($_POST['uLoc'], $_POST['uMas'], $_POST['uName'])) {
+      if (insertUniversities($_POST['uLoc'], $_POST['uMas'], $_POST['uName'], $_POST['ucid'])) {
         echo '<div class="alert alert-success" role="alert">University Added</div>';
       } else {
           echo '<div class="alert alert-danger" role="alert">Error</div>';
